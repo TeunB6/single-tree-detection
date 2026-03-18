8588366c853394e741e0d2e26f5ef7778599a9b2
richardharnisch <richitdd@gmail.com>
2026-03-18 11:30:53 +0100

    fix: singleton meta class

--- src/utils/singleton.py
+++ src/utils/singleton.py
@@ -8,3 +8,5 @@ class SingletonMeta(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
